3bed039403588f26d8a1169833f31a84de1bd368
Nguyễn Hưng <125659908+HungIT0312@users.noreply.github.com>
2023-03-19 15:35:58 +0700

    Feat/pm UI detail (#6)
    * feat/PM-UI-detail: config
    
    * feat/PM-UI-detail:Create MovieDetails UI, add API getMovieByID, bug: getGenre render
    
    * feat/PM-UI-detail: fix bug
    
    ---------
    
    Co-authored-by: NguyenHung0312 <heo7674@gmail.com>

--- Components/MovieDetails.js
+++ Components/MovieDetails.js
@@ -1,12 +1,103 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Linking, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { getMovieById } from "../api/movies";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import { IMAGE_POSTER_URL } from "../helper/config";
+import Icon from "react-native-vector-icons/AntDesign";
+import IconIos from "react-native-vector-icons/Ionicons";
+import MovieDetailStyles from "../styles/MovieDetailStyles";
+import Genres from "./Genres";
+import Constants from "../constants/constants";
+// const MovieDetails = (props) => {
 
 const MovieDetails = () => {
-  return (
-    <View>
-      <Text>MovieDetails</Text>
-    </View>
-  )
-}
+  const [movieID, setMovieID] = useState("");
+  const [isLiked, setIsLiked] = useState(false);
+  const [details, setDetails] = useState([]);
 
-export default MovieDetails
+  //maybe delete
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const _getMovie = async () => {
+      const res = await getMovieById("2");
+      setDetails(res);
+    };
+    _getMovie();
+  }, []);
+
+  return (
+    <View style={MovieDetailStyles.sectionBg}>
+      <View style={MovieDetailStyles.arrowBack}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <Icon name="arrowleft" color={Constants.textColor} size={30}></Icon>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Image
+          style={MovieDetailStyles.imageBg}
+          source={{ uri: `${IMAGE_POSTER_URL}${details.backdrop_path}` }}
+        ></Image>
+      </View>
+      <Text style={MovieDetailStyles.detailsMovieTitle}>
+        {details.original_title}
+      </Text>
+      <View style={MovieDetailStyles.linkContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL(details.homepage);
+          }}
+        >
+          <Icon name="link" color={Constants.textColor} size={22}></Icon>
+        </TouchableOpacity>
+      </View>
+      <View style={MovieDetailStyles.likeButton}>
+        <TouchableOpacity onPress={() => setIsLiked((isLiked) => !isLiked)}>
+          {isLiked ? (
+            <IconIos
+              name="heart-circle-outline"
+              color={Constants.likeButtonColor}
+              size={40}
+            ></IconIos>
+          ) : (
+            <IconIos
+              name="heart-dislike-circle-outline"
+              color={Constants.textColor}
+              size={40}
+            ></IconIos>
+          )}
+        </TouchableOpacity>
+      </View>
+      <Text style={MovieDetailStyles.heading}>OVERVIEW</Text>
+      <Text style={MovieDetailStyles.overview}>{details.overview}</Text>
+      <View style={MovieDetailStyles.detailsContainer}>
+        <View>
+          <Text style={MovieDetailStyles.heading}>BUDGET</Text>
+          <Text style={MovieDetailStyles.details}>$ {details.budget}</Text>
+        </View>
+        <View>
+          <Text style={MovieDetailStyles.heading}>DURATION</Text>
+          <Text style={MovieDetailStyles.details}>{details.runtime} min.</Text>
+        </View>
+        <View>
+          <Text style={MovieDetailStyles.heading}>RELEASE DATE</Text>
+          <Text style={MovieDetailStyles.details}>{details.release_date}</Text>
+        </View>
+      </View>
+      <Text style={MovieDetailStyles.heading}>GENRE</Text>
+      <View style={{ display: "flex", flexDirection: "row" }}>
+        {details?.genres?.length &&
+          details.genres.map((genre) => (
+            <Genres key={genre.id} genre={genre} />
+          ))}
+      </View>
+    </View>
+  );
+};
+
+export default MovieDetails;
---
+++ constants/constants.js
@@ -0,0 +1,9 @@
+const Constants = {
+    textColor: '#fff',
+    baseColor: '#151C26',
+    fadedColor: '#969696',
+    secondaryColor: '#F4C10F',
+    likeButtonColor:'#ecadb5'
+  };
+  
+  export default Constants;
